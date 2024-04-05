0a25b05e7ca7cf79e3555e5582225751ca659b3c
Burak Orkmez <burakorkmezz@gmail.com>
2024-04-05 11:56:40 +0300

    notes added to auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,10 @@
 const authConfig = {
 	providers: [
 		{
-			domain: "https://stirred-clam-37.clerk.accounts.dev",
+			// Here I'm getting the domain value from process.env.CLERK_ISSUER_URL. If you want to do the same thing set this as an environment variable in your Convex Dashboard
+			// This is something I updated after the video. The reason is: we would like to keep our secrets out of our codebase.
+			//
+			domain: process.env.CLERK_ISSUER_URL,
 			applicationID: "convex",
 		},
 	],
